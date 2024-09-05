f45553ed1ceb697ab85678ade912d0395e81fe91
Jose Ortiz <me@mdcdev.me>
2024-09-05 01:20:02 -0500

    chore: Update favicon.svg with new colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,25 @@ export const metadata: Metadata = {
   title: "Furry Bot",
   description: "A Discord bot for furries",
   icons: "/favicon.svg",
+  applicationName: "Furry Bot",
+  authors: [{ name: "Any Dev Code", url: "https://anydevcode.com" }],
+  generator: "Next.js",
+  keywords: ["discord", "bot", "furry", "images", "search"],
+  viewport: "width=device-width, initial-scale=1.0",
+  themeColor: "#edac87",
+  colorScheme: "dark",
+  creator: "Any Dev Code",
+  publisher: "Any Dev Code",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    siteName: "Furry Bot",
+    title: "Furry Bot",
+    description: "A Discord bot for furries",
+    images: [{ url: "/logo.png", alt: "Furry Bot", type: "image/png" }, { url: "/favicon.svg", alt: "Furry Bot", type: "image/svg" }],
+    url: "https://furry.anydevcode.com",
+  },
+  
 };
 
 export default function RootLayout({
